e098e764b7b067ae8355c1ca85a188a0f51c4b2b
Reynir Björnsson <reynir@reynir.dk>
2022-04-11 17:54:36 +0100

    Send NS when going from unspecified to specified

--- src/ipv6/ndpv6.ml
+++ src/ipv6/ndpv6.ml
@@ -321,7 +321,7 @@ module AddressList = struct
             Some (Int64.add now preferred_lifetime, valid_lifetime)
         in
         Log.debug (fun f -> f "SLAAC: %a --> PREFERRED" Ipaddr.pp ip);
-        Some (ip, PREFERRED timeout), []
+        Some (ip, PREFERRED timeout), [SendNS (`Specified, dst, ip)]
       else
         let dst = Ipaddr.Prefix.network_address solicited_node_prefix ip in
         Some (ip, TENTATIVE (timeout, n+1, Int64.add now retrans_timer)),
